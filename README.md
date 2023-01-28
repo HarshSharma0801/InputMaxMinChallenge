cc88cbd192780905bfc43f88ad34936ef0c0501b
HarshSharma0801 <121893196+HarshSharma0801@users.noreply.github.com>
2023-01-29 01:05:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# InputMaxMinChallenge
+ 
---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       int  count = 1;
+       double  sum = 0;
+        double  a = 0;
+        double  b = 0 ;
+        double  h = 0;
+        double  i = 0;
+        double  j = 0;
+        double  sum1 =0;
+        Scanner scanner = new Scanner(System.in);
+        while(count==1) {
+            System.out.println("Enter number #"+count+" : ");
+
+            try {
+                String m = scanner.nextLine();
+                double  n = Double .parseDouble(m);
+                sum = n ;
+                a=sum;
+                b=sum;
+                h = Double .max(sum,a);
+                i = Double .min(sum,b);
+
+
+            } catch (NumberFormatException BadUserData) {
+                System.out.println("Maximum Number Entered by you is "+h);
+                System.out.println("Minimum Number Entered by you is "+i);
+
+                break;
+            }
+            count++;
+        }
+        while(count>1) {
+            System.out.println("Enter number #"+count+" : ");
+
+            try {
+                String p = scanner.nextLine();
+                double  q = Double .parseDouble(p);
+                sum1=q;
+                h = Double .max(a,sum1);
+                i = Double .min(b,sum1);
+                a=h;
+                b=i;
+
+
+            } catch (NumberFormatException BadUserData) {
+                System.out.println("Maximum Number Entered by you is "+h);
+                System.out.println("Minimum Number Entered by you is "+i);
+
+                break;
+            }
+            count++;
+        }
+
+
+
+
+        }
+
+    }
